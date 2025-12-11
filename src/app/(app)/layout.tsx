3c982b37d67afd934c9b186aa063fa61527705a6
Jerome <nietsemorej@gmail.com>
2025-12-11 10:56:08 -0800

    Update app icons and add meta tag for mobile web app title
    - Updated icon1.png, web-app-manifest-192x192.png, web-app-manifest-512x512.png, and favicon.ico with new versions.
    - Added a meta tag for the mobile web app title in layout.tsx to enhance branding for the Everlove Charity Foundation.

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -50,6 +50,7 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
     >
       <head>
         <InitTheme />
+        <meta name="apple-mobile-web-app-title" content="Everlove Charity Foundation" />
         <link href="/favicon.ico" rel="icon" sizes="32x32" />
         <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
       </head>
